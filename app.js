573f62e31033c07115399fe4e13981f5a475908c
ViniSestari <vinifujisestari@gmail.com>
2025-06-10 19:50:26 -0300

    criaçao de cartao de filme

--- app.js
+++ app.js
@@ -10,34 +10,44 @@ let filme1 = {
 let filme2 = {
     titulo: "Karate Kid Lendas",
     foto: "filme2.jpg",
-    avaliacao: "08/10",
+    avaliacao: "06/10",
     duracao: "1h30m"
 }
 
 let filme3 = {
     titulo: "Lilo e Stitch",
     foto: "filme3.jpg",
-    avaliacao: "09/10",
+    avaliacao: "07/10",
     duracao: "2h00m"
 }
 
 let filme4 = {
     titulo: "Pecadores",
     foto: "filme4.jpg",
-    avaliacao: "07/10",
+    avaliacao: "05/10",
     duracao: "2h30m"
+}
+let filme5 = {
+    titulo: "Projeto X",
+    foto: "filme5.jpg",
+    avaliacao: "10/10",
+    duracao: "1h45m"
 }
 
 function fnMontarCartao(filmeAtual){
-    console.log(`
+    document.querySelector(".lista-filmes").innerHTML += `
         <div class= "card-filme">
         <img src="img/${filmeAtual.foto}">
         <h3>${filmeAtual.titulo}</h3>
         <span>⭐${filmeAtual.avaliacao}</span>
         </div>
-        `)
+        `
         
 }
 
+//DOM - Document Object Model
 fnMontarCartao(filme1)
 fnMontarCartao(filme2)
+fnMontarCartao(filme3)
+fnMontarCartao(filme4)
+fnMontarCartao(filme5)
