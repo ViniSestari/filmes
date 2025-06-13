ef243aa95f1b33b2155785672170bc0372c89aeb
ViniSestari <vinifujisestari@gmail.com>
2025-06-12 21:40:42 -0300

    exibição de filmes da base de dados

--- app.js
+++ app.js
@@ -1,48 +1,18 @@
-// JSON - javascript object notation
+async function fnPegarFilmes(){
+    let filmes = await fetch ("dados-filmes.json")
+    let filmesTratados = await filmes.json()
+    filmesTratados.slice(0,4).forEach((filmeAtual)=>{
+        document.querySelector(".lista-filmes").innerHTML += `
+        <div class="card-filme">
+            <img src="${filmeAtual.foto}">
+            <h3>${filmeAtual.titulo}</h3>
+            <span>⭐ ${filmeAtual.avaliacao}</span>
+    </div>
+    `
+    })
+    }
+    
+    fnPegarFilmes()
 
-let filme1 = {
-    titulo: "Jurassic Park",
-    foto: "filme1.webp",
-    avaliacao: "10/10",
-    duracao: "1h50m"
-}
-
-let filme2 = {
-    titulo: "Karate Kid Lendas",
-    foto: "filme2.jpg",
-    avaliacao: "06/10",
-    duracao: "1h30m"
-}
-
-let filme3 = {
-    titulo: "Lilo e Stitch",
-    foto: "filme3.jpg",
-    avaliacao: "07/10",
-    duracao: "2h00m"
-}
-
-let filme4 = {
-    titulo: "Pecadores",
-    foto: "filme4.jpg",
-    avaliacao: "05/10",
-    duracao: "2h30m"
-}
-let filme5 = {
-    titulo: "Projeto X",
-    foto: "filme5.jpg",
-    avaliacao: "10/10",
-    duracao: "1h45m"
-}
-
-//DOM - Document Object Model
-let todosOsFilmes = [filme1, filme2, filme3, filme4, filme5]
-
-todosOsFilmes.forEach((filmeAtual) => {
-    document.querySelector(".lista-filmes").innerHTML += `
-    <div class="card-filme">
-        <img src="img/${filmeAtual.foto}">
-        <h3>${filmeAtual.titulo}</h3>
-        <span>⭐ ${filmeAtual.avaliacao}</span>
-</div>
-`
-})
+   
+    
--- app2.js
+++ app2.js
@@ -2,9 +2,21 @@
 async function fnPegarFilmes(){
 let filmes = await fetch ("dados-filmes.json")
 let filmesTratados = await filmes.json()
-dilmesTratados.forEach((filmeAtual)=>{
-    console.log(filmeAtual)
+filmesTratados.forEach((filmeAtual)=>{
+    document.querySelector(".lista-filmes").innerHTML += `
+    <div class="card-filme">
+        <img src="${filmeAtual.foto}">
+        <h3>${filmeAtual.titulo}</h3>
+        <span>⭐ ${filmeAtual.avaliacao}</span>
+</div>
+`
 })
+}
 
 fnPegarFilmes()
 console.log("oi")
+
+
+
+
+
